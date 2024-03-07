b00dc59786429341fd52df90946a89643c04c3a8
Bruno Barros <brunobarros2093@gmail.com>
2024-03-06 23:13:10 -0300

    first test

--- src/test/java/com/example/tests/CalculatorTeste.java
+++ src/test/java/com/example/tests/CalculatorTeste.java
@@ -1,5 +1,14 @@
 package com.example.tests;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CalculatorTeste {
     
+    @Test
+    public void testSum() {
+        Calculator calculator = new Calculator();
+        Assertions.assertThat(calculator.sum(2, 2)).isEqualTo(4);
+    }
+
 }
