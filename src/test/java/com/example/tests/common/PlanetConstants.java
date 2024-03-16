501b5a786f4d3752d21520861f1042187644cc78
Bruno Barros <brunobarros2093@gmail.com>
2024-03-15 23:44:26 -0300

    Thorws error

--- src/test/java/com/example/tests/common/PlanetConstants.java
+++ src/test/java/com/example/tests/common/PlanetConstants.java
@@ -5,6 +5,7 @@ import com.example.tests.domain.Planet;
 public class PlanetConstants {
 
     public static final Planet PLANET = new Planet("name", "climate", "terrain");
+    public static final Planet INVALID_PLANET = new Planet("", "", "");
 
 
 }
--- src/test/java/com/example/tests/domain/PlanetServiceTest.java
+++ src/test/java/com/example/tests/domain/PlanetServiceTest.java
@@ -1,23 +1,31 @@
 package com.example.tests.domain;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import static com.example.tests.common.PlanetConstants.INVALID_PLANET;
 import static com.example.tests.common.PlanetConstants.PLANET;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest(classes = PlanetService.class)
+//@SpringBootTest(classes = PlanetService.class)
+@ExtendWith(MockitoExtension.class)
 public class PlanetServiceTest {
 
-    @Autowired
+    @InjectMocks
     private PlanetService planetService;
-
-    @MockBean
+    @Mock
     private PlanetRepository planetRepository;
+
+
     @Test
     public void createPlanet_WithValiddate_ReturnsPlanet() {
         // AAA - Arrange, Act, Assert
@@ -26,6 +34,13 @@ public class PlanetServiceTest {
         Planet sut = planetService.create(PLANET);
         // ASSERT
         assertThat(sut).isEqualTo(PLANET);
+
+    }
+
+    @Test
+    public void createPlanet_WithInvalidData_ThrowsException() {
+        when(planetRepository.save(INVALID_PLANET)).thenThrow(RuntimeException.class);
+        assertThatThrownBy(()->planetService.create(INVALID_PLANET)).isInstanceOf(RuntimeException.class);
 
     }
 
